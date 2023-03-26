63789d73479a29f55e50c0c957c7e6e8d3bbb5ec
Rolo365 <jmoralesc2005@alumno.ipn.mx>
2023-03-26 12:30:13 -0600

    material ui

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,12 +1,26 @@
 import React, { useContext, useEffect } from 'react'
 import { Context } from '../context/useReducer'
+import { TextField } from '@mui/material'
 
 const Index = () => {
 
     const [state, dispatch] = useContext(Context)
 
     return (
-        <div>Hola</div>
+        <div className="background background-page background-scroll background-white-to-green d-grid align-items-center px-3">
+            <div>
+                <div className="card-local card-xl color-primary card-login">
+                    <div className='background background-grey p-5'>
+                        <div>
+                            Hola
+                        </div>
+                    </div>
+                    <div className='first background background-white p-5'>
+                        <img src='../../public/images/svg/login.svg' alt="login" className='w-100' />
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 

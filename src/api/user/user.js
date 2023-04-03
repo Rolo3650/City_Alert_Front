a304f6a50e08dd553418c3e4b95d7be130a4bb1d
Rolo365 <jmoralesc2005@alumno.ipn.mx>
2023-04-02 22:24:30 -0600

    fixes

--- src/api/user/user.js
+++ src/api/user/user.js
@@ -1,9 +1,11 @@
 import api from "../index";
 
 const GET_USERS = () => api.get('get-users');
-const LOGIN = (params) => api.post('sign-in', params)
+const LOGIN = (params) => api.post('sign-in', params);
+const GET_SEXS = () => api.get('get-sexs');
 
 export {
     GET_USERS,
-    LOGIN
+    LOGIN,
+    GET_SEXS
 };

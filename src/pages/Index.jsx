d695e34446b20445d43cef65097634099e04785f
Rolo365 <jmoralesc2005@alumno.ipn.mx>
2023-04-20 18:37:47 -0600

    fix

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { LoginForm } from '../containers/user/login/LoginFormat'
+import { LoginForm } from '../containers/user/login/LoginForm'
 import { useUser } from '../hooks/user/useUser'
 import { useNavigate } from 'react-router-dom'
 import { Backdrop, CircularProgress, Link, Skeleton } from '@mui/material'
